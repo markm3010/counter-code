6e273d02d9b8bf8d2be798784f73034971e97b7b
markm3010 <65625887+markm3010@users.noreply.github.com>
2021-04-19 07:57:30 -0500

    Added the "selfishness index" script!  It searches for typical selfish indication words we all use, but some use them more than others.  It uses "random-blog.txt".  Note when copy-pasting from a website to a file, it's best to change the encoding to "ansi", else the apostrophe is liable to be unrecognizable by the script.  I should fix that.

---
+++ count_self-centeredness.py
@@ -0,0 +1,24 @@
+import re
+
+
+def selfish_meter(file_in):
+    fh = open(file_in, "r")
+    data = fh.read()
+    fh.close()
+    # print(data)
+
+    searcher = re.compile(r'\bgI\b|\bI\'ve\b|\bI\'[md]\b|\bmy\b|\bmine\b')
+    matches = searcher.finditer(data)
+    i = 0
+    print('\nSelfish words found:')
+    for match in matches:
+        print(match.group(0), end=' ')
+        i += 1
+
+    return i
+
+
+if __name__ == '__main__':
+    filename = "random-blog.txt"
+    count = selfish_meter(filename)
+    print(f'\n\nnumber of selfish words found: {count}')
